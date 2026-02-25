ea067d6087eeb8a83aabec5d0824c093e76fec95
vivi29 <vivekroy2964@gmail.com>
2026-02-25 22:34:43 +0530

    learned validate method

--- wallet.py
+++ wallet.py
@@ -9,3 +9,36 @@ class Wallet:
    def withdraw(self, amount):
        if 0 < amount <= self._balance:
            self._balance -= amount # Remove from the balance safely
+
+
+class Wallet:
+   def __init__(self):
+       self.__balance = 0
+
+   def __validate(self, amount):
+       if amount < 0:
+           raise ValueError('Amount must be positive')
+
+   def deposit(self, amount):
+       self.__validate(amount)
+       self.__balance += amount
+
+   def withdraw(self, amount):
+       self.__validate(amount)
+       if amount > self.__balance:
+           raise ValueError('Insufficient funds')
+       self.__balance -= amount
+
+   def get_balance(self):
+       return self.__balance
+
+acct_one = Wallet()
+acct_one.deposit(3)
+print(acct_one.get_balance()) # 3
+
+acct_one.deposit(50)
+print(acct_one.get_balance()) # 53
+
+acct_one.deposit(-4)  # ValueError: Amount must be positive
+acct_one.withdraw(-8) # ValueError: Amount must be positive
+acct_one.withdraw(58) # ValueError: Insufficient funds
