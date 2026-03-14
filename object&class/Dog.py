7f2f83042eb1c4bb520c5e25834d97fe6b3f0464
vivi29 <vivekroy2964@gmail.com>
2026-03-14 23:35:31 +0530

    started class & Object

---
+++ object&class/Dog.py
@@ -0,0 +1,13 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def bark(self):
+        print(f"{self.name.upper()} says woof woof! I'm {self.age} years old!")
+
+dog_1 = Dog("Jack", 3)
+dog_2 = Dog("Thatcher", 5)
+
+dog_1.bark()
+dog_2.bark()
