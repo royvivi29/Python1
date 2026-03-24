d56145419ec18304ae79775ac1e60de58f441c38
vivi29 <vivekroy2964@gmail.com>
2026-03-24 23:40:32 +0530

    instance and class attributes

--- object&class/Dog.py
+++ object&class/Dog.py
@@ -11,3 +11,20 @@ dog_2 = Dog("Thatcher", 5)
 
 dog_1.bark()
 dog_2.bark()
+
+#  instance and class attributes
+class Dog1:
+    species = "French Bulldog" #class Attribute 
+
+    def __init__(self, name):
+        self.name = name   #Instance attribute
+
+print(Dog1.species)
+
+dog1_1 = Dog1("Jack")
+print(dog1_1.name)
+print(dog1_1.species)
+
+dog2_1 = Dog1("Tom")
+print(dog2_1.name)
+print(dog2_1.species)
